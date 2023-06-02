96ff69793f8bc00bdc34cfe64e2dce1c93c0a45b
“wilvargas” <wilvargas7gmail.com>
2023-06-02 12:55:31 -0500

    Realicé una modificación en los textos de descripción para practicar

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,7 +1,7 @@
 function next_pal(num) {
-    num++; // Incrementar el número en 1
+    num++; // Incrementa el número en 1
     while (true) {
-        if (esPalindromo(num.toString())) { // Verificar si el número es palíndromo
+        if (esPalindromo(num.toString())) { // Verifica si el número es palíndromo
         return num;
         }
     num++;
@@ -9,7 +9,7 @@ function next_pal(num) {
   }
   
   function esPalindromo(str) {
-  // Verificar si una cadena es palíndromo
+  // Verifica si una cadena es palíndromo
     const reversar = str.split('').reverse().join('');
     return str === reversar;
   }
